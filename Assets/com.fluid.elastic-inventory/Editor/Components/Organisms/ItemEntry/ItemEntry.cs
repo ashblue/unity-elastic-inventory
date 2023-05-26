a04ca334b141c96f4a1465ffaf604c4a25002e4d
Ash Blue <ash@cased.com>
2023-05-26 16:58:24 -0700

    chore: item entry editor window text is now binded to the display name of the definition

--- Assets/com.fluid.elastic-inventory/Editor/Components/Organisms/ItemEntry/ItemEntry.cs
+++ Assets/com.fluid.elastic-inventory/Editor/Components/Organisms/ItemEntry/ItemEntry.cs
@@ -1,15 +1,14 @@
 using UnityEditor;
-using UnityEngine;
+using UnityEditor.UIElements;
 using UnityEngine.UIElements;
 
 namespace CleverCrow.Fluid.ElasticInventory.Editors {
     public class ItemEntry : ComponentBase {
         public ItemEntry (VisualElement container, ItemDefinitionBase definition) : base(container) {
-            _container.Q<Label>("item-entry__name").text = definition.DisplayName;
-
-            _container.Q<Image>("item-entry__image").image = definition.Image != null
-                ? definition.Image.texture
-                : AssetDatabase.LoadAssetAtPath<Texture2D>($"{AssetPath.BasePath}/com.fluid.elastic-inventory/Images/PlaceholderImage.png");
+            var title = _container.Q<Label>("item-entry__name");
+            title.text = definition.DisplayName;
+            title.bindingPath = "_displayName";
+            title.Bind(new SerializedObject(definition));
 
             _container.Q<Button>("item-entry__edit").clicked += () => { Selection.activeObject = definition; };
         }
